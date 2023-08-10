225109b18e423435842c4226d7d44ff551d56d2b
Blizz <emmanuelblizz@gmail.com>
2023-08-10 06:52:57 -0700

    using puts not printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#!bin/bash
 #include <stdio.h>
 /**
- * main - First program
- *
+ * main -A program using puts
  * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
