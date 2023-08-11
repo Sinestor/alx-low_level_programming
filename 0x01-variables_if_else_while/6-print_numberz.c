14b333b85da189b0824764512e6cb39af3f54dcb
Blizz <emmanuelblizz@gmail.com>
2023-08-11 11:13:43 -0700

    prints the numbers 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int e;
+
+	for (e = 48; e < 58; e++)
+	{
+		putchar(e);
+	}
+	putchar('\n');
+	return (0);
+}
+
