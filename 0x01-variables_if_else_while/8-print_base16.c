52137908f41e8dee809a5775a13e2ac9218123e8
Blizz <emmanuelblizz@gmail.com>
2023-08-11 11:29:53 -0700

    prints the alphabets and numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b < 58; b++)
+	{
+		putchar(b);
+	}
+	for (b = 97; b < 103; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
