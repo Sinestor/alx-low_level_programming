29e243c12e965cecce78dbbe27ceb3b05e720d18
Blizz <emmanuelblizz@gmail.com>
2023-08-15 13:14:15 -0700

    a function that prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - a function that prints 10 times the alphabet, in lowercase
+* print_alphabet_x10 - function that prints 10 times the alphabet
 * Return: void
 */
 
