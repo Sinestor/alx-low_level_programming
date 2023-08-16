ce2bbd9dad739c55d9dab613d05239c000b632bf
Blizz <emmanuelblizz@gmail.com>
2023-08-16 00:19:03 -0700

    a function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,6 +3,7 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
+ * Return: void
  */
 void times_table(void)
 {
@@ -26,6 +27,7 @@ void times_table(void)
 	_putchar(' ');
 	_putchar(' ');
 	_putchar(k + '0');
+	}
 	 else if (k >= 10)
 	{
 	_putchar(',');
