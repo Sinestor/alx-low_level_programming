f1afab9a8066b178a3fa51a4e6c80fc7f51016cd
Sinestor <emmanuelblizz722@gmail.com>
2023-09-02 08:59:56 -0700

    a program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
- * main - Write a program that prints all arguments it receives
+/**
+ * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  *
