740ebc65ee256af89752504d7763c172236bcc4b
Sinestor <emmanuelblizz722@gmail.com>
2023-09-02 06:56:17 -0700

    Create the static library libmy.a containing all the functions listed below

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,28 +1,21 @@
 #include "main.h"
- /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
- */
-char *_strcat(char *dest, char *src)
-{
-	int i;
-	int j;
+#include <stddef.h>
 
-	i = 0;
-	while (dest[i] != '\0')
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
 	{
-		i++;
+		if (s[i] == c)
+			return (&s[i]);
 	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return (0);
 }
+
