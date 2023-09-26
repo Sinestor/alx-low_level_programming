6adf713871ceaa8797fe79034cbe66685f6a26c7
Sinestor <emmanuelblizz722@gmail.com>
2023-09-26 11:58:00 -0700

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint2 - this free's a linked list
+ * free_listint2 - this frees a linked list
  * @head: a pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
@@ -17,4 +17,4 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
-
+}
