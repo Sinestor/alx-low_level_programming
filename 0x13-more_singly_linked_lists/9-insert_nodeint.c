4cfedb4c1a417b4f084a331e3f65cf77dd94bfbd
Sinestor <emmanuelblizz722@gmail.com>
2023-09-27 13:07:52 -0700

    a function that deletes the node at index index of a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: an index where the new node is added
+ * @n: a data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *node;
+	listint_t *temp = *head;
+
+	node = malloc(sizeof(listint_t));
+	if (!node || !head)
+		return (NULL);
+
+	node->n = n;
+	node->next = NULL;
+
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+
+	for (a = 0; temp && a < idx; a++)
+	{
+		if (a == idx - 1)
+		{
+			node->next = temp->next;
+			temp->next = node;
+			return (node);
+		}
+		else
+			temp = temp->next;
+	}
+
+	return (NULL);
+}
